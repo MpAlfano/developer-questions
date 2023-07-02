872707f4346d9cb912ac0ea768b5f1913314b576
Mark Alfano <mpalfano4@gmail.com>
2023-07-02 16:13:49 -0400

    question 2 passing tests

--- question2/main.ts
+++ question2/main.ts
@@ -1,1 +1,9 @@
-export function findOutlier(integers: number[]): number {}
+export function findOutlier(integers: number[]): number {
+  let checkEven = true;
+
+  if ((integers[0] % 2) + (integers[1] % 2) + (integers[2] % 2) >= 2) {
+    checkEven = false;
+  }
+
+  return integers.filter((num) => (num % 2 === 0) !== checkEven)[0];
+}
