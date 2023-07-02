ec0e857a41ae17814eea817f84f96ba906ed3b61
Mark Alfano <mpalfano4@gmail.com>
2023-07-02 16:10:27 -0400

    first question passing tests

--- question1/main.ts
+++ question1/main.ts
@@ -1,1 +1,15 @@
-export function isValidWalk(walk: string[]): boolean {}
+export function isValidWalk(walk: string[]): boolean {
+  const walkObj = { n: 0, s: 0, w: 0, e: 0 };
+
+  if (walk.length !== 10) return false;
+
+  for (let i = 0; i < walk.length; i++) {
+    walkObj[walk[i]]++;
+  }
+
+  if (walkObj["n"] === walkObj["s"] && walkObj["w"] === walkObj["e"]) {
+    return true;
+  }
+
+  return false;
+}
